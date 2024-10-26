238a12be3597c51359b9c0ab4018e670a78e2ad0
Agil Setiawan <agilsetiawan772@gmail.com>
2024-10-26 15:18:14 +0700

    Fix quick action button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,67 +11,79 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void openFAB() {
+      final state = _key.currentState;
+      if (state != null) {
+        state.toggle();
+      }
+    }
+
     return Scaffold(
       floatingActionButtonLocation: ExpandableFab.location,
-      floatingActionButton: InkWell(
-        onLongPress: () {
-          print('longpress: add expense');
-        },
-        child: ExpandableFab(
-          key: _key,
-          type: ExpandableFabType.fan,
-          pos: ExpandableFabPos.center,
-          fanAngle: 180,
-          duration: const Duration(milliseconds: 100),
-          openButtonBuilder: RotateFloatingActionButtonBuilder(
-            shape: const CircleBorder(),
-            child: const Icon(
-              TablerIcons.plus,
+      floatingActionButton: ExpandableFab(
+        key: _key,
+        type: ExpandableFabType.fan,
+        pos: ExpandableFabPos.center,
+        fanAngle: 180,
+        duration: const Duration(milliseconds: 100),
+        openButtonBuilder: RotateFloatingActionButtonBuilder(
+          shape: const CircleBorder(),
+          child: InkWell(
+            customBorder: const CircleBorder(),
+            onLongPress: () {
+              debugPrint('quick action');
+            },
+            onTap: openFAB,
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              decoration: const BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(TablerIcons.plus),
             ),
           ),
-          closeButtonBuilder: DefaultFloatingActionButtonBuilder(
-            shape: const CircleBorder(),
-            child: const Icon(
-              TablerIcons.x,
-            ),
-          ),
-          onOpen: () {},
-          onClose: () {},
-          overlayStyle: const ExpandableFabOverlayStyle(
-            color: Colors.black38,
-            blur: 1.2,
-          ),
-          children: [
-            Column(
-              children: [
-                const Text('Income'),
-                const Gap(10),
-                FloatingActionButton(
-                  shape: const CircleBorder(),
-                  child: const Icon(
-                    TablerIcons.arrow_bar_to_down,
-                    color: Colors.green,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                const Text('Expense'),
-                const Gap(10),
-                FloatingActionButton(
-                  shape: const CircleBorder(),
-                  child: const Icon(
-                    TablerIcons.arrow_bar_up,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
         ),
+        closeButtonBuilder: DefaultFloatingActionButtonBuilder(
+          shape: const CircleBorder(),
+          child: const Icon(TablerIcons.x),
+        ),
+        onOpen: () {},
+        onClose: () {},
+        overlayStyle: const ExpandableFabOverlayStyle(
+          color: Colors.black38,
+          blur: 1.2,
+        ),
+        children: [
+          Column(
+            children: [
+              const Text('Income'),
+              const Gap(10),
+              FloatingActionButton(
+                shape: const CircleBorder(),
+                child: const Icon(
+                  TablerIcons.arrow_bar_to_down,
+                  color: Colors.green,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text('Expense'),
+              const Gap(10),
+              FloatingActionButton(
+                shape: const CircleBorder(),
+                child: const Icon(
+                  TablerIcons.arrow_bar_up,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
       ),
       body: Center(
         child: Text(
