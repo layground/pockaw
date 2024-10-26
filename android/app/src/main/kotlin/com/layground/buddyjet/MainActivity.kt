a38e54ba9144e1d7d46c7d10077b28ca7ad2be59
Agil Setiawan <agilsetiawan772@gmail.com>
2024-10-26 12:24:19 +0700

    Apply latest flutter build settings for all platforms

---
+++ android/app/src/main/kotlin/com/layground/buddyjet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.layground.buddyjet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/id/layground/buddyjet/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package id.layground.buddyjet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
