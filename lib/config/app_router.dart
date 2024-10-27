fdb6ef49bae3abdbaeb8484d35fa6ec44c2ccaa6
Agil Setiawan <agilsetiawan772@gmail.com>
2024-10-27 17:57:03 +0700

    Create app_router.dart

---
+++ lib/config/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:buddyjet/ui/screens/expense_form_screen/expense_form_screen.dart';
+import 'package:buddyjet/ui/screens/home_screen/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  initialLocation: AppRouter.index,
+  routes: [
+    GoRoute(
+      path: AppRouter.index,
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: AppRouter.expenseForm,
+      builder: (context, state) => const ExpenseFormScreen(),
+    ),
+  ],
+);
+
+class AppRouter {
+  static const String index = '/';
+  static const String expenseForm = '/expense-form';
+}
