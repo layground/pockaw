a16608c40f0089df38320aff24e9f0ce88da24c8
Agil Setiawan <agilsetiawan772@gmail.com>
2024-11-30 18:26:58 +0700

    Remove status bar background

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
-import 'package:pockaw/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pockaw/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // Makes the status bar transparent.
+      statusBarIconBrightness: Brightness.dark, // Adjust for light/dark icons.
+    ),
+  );
   runApp(const MyApp());
 }
