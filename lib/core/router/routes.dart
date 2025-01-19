fcbc8b32d93c81fc4f7d0b37e3aa00171c2210f7
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-19 16:40:52 +0700

    Update routes.dart

--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,6 +1,7 @@
 class Routes {
   static const String index = '/';
   static const String login = '/login';
+  static const String currencyListTile = '/currency';
   static const String main = '/main';
   static const String transactionForm = '/transaction-form';
 }
