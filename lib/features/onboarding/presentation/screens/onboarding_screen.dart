3d08d34b363e301685e9f2f69c04885a5f221f4e
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-19 16:43:43 +0700

    Update onboarding_screen.dart

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -3,6 +3,7 @@ import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pockaw/core/components/buttons/buttons.dart';
 import 'package:pockaw/core/constants/app_colors.dart';
+import 'package:pockaw/core/constants/app_text_styles.dart';
 import 'package:pockaw/core/router/routes.dart';
 
 part '../components/get_started_button.dart';
@@ -31,29 +32,19 @@ class OnboardingScreen extends StatelessWidget {
                 const Gap(16),
                 const Text(
                   'Welcome to',
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontVariations: [FontVariation.weight(900)],
-                    color: AppColors.primary900,
-                    height: 1,
-                  ),
+                  style: AppTextStyles.heading2,
                 ),
                 const Text(
                   'Pockaw!',
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontVariations: [FontVariation.weight(900)],
-                    color: AppColors.secondary,
-                  ),
+                  style: AppTextStyles.heading2,
                 ),
                 const Gap(16),
-                const Text(
+                Text(
                   'Simple and intuitive finance buddy. Track your expenses, set goals, '
                   'organize your pocket and wallet sized finance — everything effortlessly. 🚀',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontVariations: [FontVariation.weight(600)],
-                    color: AppColors.primary700,
+                  style: AppTextStyles.body1.copyWith(
+                    color: AppColors.neutral800,
+                    fontVariations: [const FontVariation.weight(500)],
                   ),
                 ),
                 const Gap(150),
