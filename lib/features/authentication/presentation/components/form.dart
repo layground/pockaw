fb6ecb23fea8e0d42256a2f203907c870a762375
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-19 16:42:41 +0700

    Update login screen

---
+++ lib/features/authentication/presentation/components/form.dart
@@ -0,0 +1,110 @@
+part of '../screens/login_screen.dart';
+
+class Form extends StatelessWidget {
+  const Form({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Get Started',
+                style: AppTextStyles.heading5,
+              ),
+              Gap(AppSpacing.spacing4),
+              Text.rich(
+                style: AppTextStyles.body3,
+                TextSpan(
+                  text: 'Please enter your ',
+                  children: [
+                    TextSpan(
+                      text: 'name or brand name',
+                      style: TextStyle(
+                          fontVariations: [FontVariation.weight(700)]),
+                    ),
+                    TextSpan(
+                      text: ', pick your best ',
+                    ),
+                    TextSpan(
+                      text: 'picture',
+                      style: TextStyle(
+                          fontVariations: [FontVariation.weight(700)]),
+                    ),
+                    TextSpan(
+                      text: ' and choose your ',
+                    ),
+                    TextSpan(
+                      text: 'currency',
+                      style: TextStyle(
+                          fontVariations: [FontVariation.weight(700)]),
+                    ),
+                    TextSpan(
+                      text: ' to personalize your account.',
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const Gap(AppSpacing.spacing20),
+          Column(
+            children: [
+              Row(
+                children: [
+                  const Expanded(
+                    child: CustomTextField(
+                      label: 'Nickname',
+                      hint: 'Enter your nickname...',
+                      icon: TablerIcons.letter_case,
+                    ),
+                  ),
+                  const Gap(AppSpacing.spacing16),
+                  InkWell(
+                    child: Container(
+                      height: 70,
+                      width: 70,
+                      padding: const EdgeInsets.all(AppSpacing.spacing20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(AppRadius.radius8),
+                        color: AppColors.light,
+                        border: Border.all(
+                          color: AppColors.neutralAlpha50,
+                        ),
+                      ),
+                      child: const Icon(TablerIcons.photo),
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(AppSpacing.spacing16),
+              const CurrencyPickerField(),
+            ],
+          ),
+          const Gap(AppSpacing.spacing20),
+          Text.rich(
+            TextSpan(
+                text: 'We only store your data into local database '
+                    'on this device. So you are in charge! ',
+                style: AppTextStyles.body4,
+                children: [
+                  TextSpan(
+                    text: 'Read more.',
+                    style: AppTextStyles.body4.copyWith(
+                      decoration: TextDecoration.underline,
+                      decorationColor: AppColors.secondary,
+                      color: AppColors.secondary,
+                    ),
+                  ),
+                ]),
+          ),
+          const Gap(AppSpacing.spacing56),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/authentication/presentation/screens/login_screen.dart
+++ lib/features/authentication/presentation/screens/login_screen.dart
@@ -1,16 +1,19 @@
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pockaw/core/components/buttons/buttons.dart';
-import 'package:pockaw/core/components/buttons/circle_button.dart';
 import 'package:pockaw/core/components/form_fields/custom_text_field.dart';
 import 'package:pockaw/core/constants/app_colors.dart';
+import 'package:pockaw/core/constants/app_constants.dart';
 import 'package:pockaw/core/constants/app_radius.dart';
 import 'package:pockaw/core/constants/app_spacing.dart';
 import 'package:pockaw/core/constants/app_text_styles.dart';
 import 'package:pockaw/core/router/routes.dart';
+import 'package:pockaw/features/currency_picker/presentation/components/currency_picker_field.dart';
+
+part '../components/form.dart';
+part '../components/logo.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -25,112 +28,15 @@ class LoginScreen extends StatelessWidget {
           Container(
             // color: Colors.yellow,
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
+            child: const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/icon/icon-transparent-full.png',
-                        width: 100,
-                      ),
-                      const Gap(AppSpacing.spacing12),
-                      const Text(
-                        'Pockaw',
-                        style: TextStyle(
-                          fontSize: 38,
-                          fontVariations: [FontVariation.weight(900)],
-                          color: AppColors.secondary,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Gap(AppSpacing.spacing48),
-                Column(
-                  children: [
-                    const Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Login',
-                          style: AppTextStyles.heading4,
-                        ),
-                        Gap(AppSpacing.spacing4),
-                        Text(
-                          'Please enter your nickname and pick your best picture to personalize your account.',
-                          style: AppTextStyles.body3,
-                        ),
-                      ],
-                    ),
-                    const Gap(AppSpacing.spacing20),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Expanded(
-                              child: CustomTextField(
-                                label: 'Nickname',
-                                hint: 'Enter your nickname...',
-                                icon: TablerIcons.letter_case,
-                              ),
-                            ),
-                            const Gap(AppSpacing.spacing16),
-                            Container(
-                              padding:
-                                  const EdgeInsets.all(AppSpacing.spacing4),
-                              decoration: BoxDecoration(
-                                color: AppColors.secondary100,
-                                borderRadius: BorderRadius.circular(
-                                  AppRadius.radius8,
-                                ),
-                              ),
-                              child: DottedBorder(
-                                borderType: BorderType.Circle,
-                                strokeCap: StrokeCap.round,
-                                dashPattern: const [8, 8],
-                                color: AppColors.primary600,
-                                child: const CircleIconButton(
-                                  icon: TablerIcons.photo,
-                                  radius: 33,
-                                  backgroundColor: AppColors.secondary100,
-                                  foregroundColor: AppColors.secondary800,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    const Gap(AppSpacing.spacing20),
-                    Text.rich(
-                      TextSpan(
-                          text: 'We only store your data into local database '
-                              'on this device. So you are in charge! ',
-                          style: AppTextStyles.body4.copyWith(
-                            color: AppColors.primary600,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'Read more.',
-                              style: AppTextStyles.body4.copyWith(
-                                decoration: TextDecoration.underline,
-                                decorationColor: Colors.blueAccent,
-                                color: Colors.blueAccent,
-                              ),
-                            ),
-                          ]),
-                    )
-                  ],
-                ),
-                const Gap(AppSpacing.spacing56),
-                const Gap(AppSpacing.spacing56),
+                Logo(),
+                Gap(AppSpacing.spacing48),
+                Form(),
+                Gap(AppSpacing.spacing56),
+                Gap(AppSpacing.spacing56),
               ],
             ),
           ),
