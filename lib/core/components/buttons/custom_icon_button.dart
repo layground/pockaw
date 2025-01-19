8f9e24ece044cbee6b90d51ada6284c6dfc6aa00
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-19 16:42:24 +0700

    Update custom_icon_button

--- lib/core/components/buttons/custom_icon_button.dart
+++ lib/core/components/buttons/custom_icon_button.dart
@@ -3,21 +3,31 @@ import 'package:pockaw/core/constants/app_colors.dart';
 import 'package:pockaw/core/constants/app_radius.dart';
 import 'package:pockaw/core/constants/app_spacing.dart';
 
-class OutlinedIconButton extends IconButton {
-  OutlinedIconButton({
+enum IconSize {
+  large,
+  medium,
+  small,
+  tiny,
+}
+
+class CustomIconButton extends IconButton {
+  CustomIconButton({
     Key? key,
     required GestureTapCallback onPressed,
     required IconData icon,
+    Color? color,
     bool showBadge = false,
+    IconSize iconSize = IconSize.medium,
   }) : super(
           key: key,
           onPressed: onPressed,
           style: IconButton.styleFrom(
             padding: const EdgeInsets.all(AppSpacing.spacing8),
+            backgroundColor: color ?? AppColors.purple50,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(AppRadius.radius8),
               side: const BorderSide(
-                color: AppColors.primary,
+                color: AppColors.purpleAlpha10,
               ),
             ),
           ),
@@ -25,8 +35,8 @@ class OutlinedIconButton extends IconButton {
             children: [
               Icon(
                 icon,
-                color: AppColors.primary,
-                size: 18,
+                color: AppColors.purple800,
+                size: _getIconSize(iconSize),
               ),
               !showBadge
                   ? const SizedBox()
@@ -38,6 +48,17 @@ class OutlinedIconButton extends IconButton {
             ],
           ),
         );
+
+  static double _getIconSize(IconSize size) => switch (size) {
+        // TODO: Handle this case.
+        IconSize.large => 26,
+        // TODO: Handle this case.
+        IconSize.medium => 22,
+        // TODO: Handle this case.
+        IconSize.small => 18,
+        // TODO: Handle this case.
+        IconSize.tiny => 14,
+      };
 
   static Widget _badge() => Container(
         width: 6,
