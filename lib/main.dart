e6a7c2ced5bde8e0f0f1c502d8b6efe14632df02
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-19 16:40:13 +0700

    Enable ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pockaw/core/app.dart';
 
 void main() {
@@ -10,5 +11,5 @@ void main() {
       statusBarIconBrightness: Brightness.dark, // Adjust for light/dark icons.
     ),
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
