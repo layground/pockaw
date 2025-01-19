60e9c58b273c9a173f6187841871b68d0655d846
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-19 18:00:26 +0700

    Update text_style_extensions.dart

--- lib/core/extensions/text_style_extensions.dart
+++ lib/core/extensions/text_style_extensions.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:pockaw/core/constants/app_font_weights.dart';
 
 extension TextStyleExtensions on TextStyle {
+  TextStyle get semibold => copyWith(
+        fontVariations: [AppFontWeights.semiBold],
+      );
+
   TextStyle get bold => copyWith(
         fontVariations: [AppFontWeights.bold],
       );
