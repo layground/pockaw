042102de50fd44fb1110980ff46f7bb401be537b
Agil Setiawan <agilsetiawan772@gmail.com>
2025-01-20 07:22:31 +0700

    Update custom_select_field.dart

--- lib/core/components/form_fields/custom_select_field.dart
+++ lib/core/components/form_fields/custom_select_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
-import 'package:pockaw/core/constants/app_colors.dart';
 import 'package:pockaw/core/components/form_fields/custom_text_field.dart';
 
 class CustomSelectField extends StatelessWidget {
@@ -34,7 +33,6 @@ class CustomSelectField extends StatelessWidget {
       hint: hint,
       hintColor: hintColor,
       suffixIcon: suffixIcon ?? TablerIcons.chevron_right,
-      border: Border.all(color: AppColors.secondary),
       readOnly: true,
       // hintColor: AppColors.dark,
     );
