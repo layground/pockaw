59d0f0626981bc7d7bcbcad33ba54857c63b06aa
Agil Setiawan <agilsetiawan772@gmail.com>
2025-03-22 12:22:45 +0700

    Add goal_screen

--- lib/features/goal/presentation/screens/goal_screen.dart
+++ lib/features/goal/presentation/screens/goal_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:gap/gap.dart';
+import 'package:pockaw/core/components/buttons/custom_icon_button.dart';
+import 'package:pockaw/core/components/goal_card/goal_card.dart';
 import 'package:pockaw/core/components/scaffolds/custom_scaffold.dart';
+import 'package:pockaw/core/constants/app_spacing.dart';
 
 class GoalScreen extends StatelessWidget {
   const GoalScreen({super.key});
@@ -9,8 +14,23 @@ class GoalScreen extends StatelessWidget {
     return CustomScaffold(
       context: context,
       showBackButton: false,
-      body: const Center(
-        child: Text('Goal'),
+      title: 'My Goals',
+      actions: [
+        CustomIconButton(
+          onPressed: () {},
+          icon: TablerIcons.plus,
+          iconSize: IconSize.medium,
+        ),
+      ],
+      body: ListView.separated(
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppSpacing.spacing20,
+          vertical: AppSpacing.spacing20,
+        ),
+        shrinkWrap: true,
+        itemCount: 3,
+        itemBuilder: (context, index) => const GoalCard(),
+        separatorBuilder: (context, index) => const Gap(AppSpacing.spacing12),
       ),
     );
   }
