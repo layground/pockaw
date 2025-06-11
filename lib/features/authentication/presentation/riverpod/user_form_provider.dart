0dbf2657c972f56472adce6d1bbb5022200cce3c
Agil Setiawan <agilsetiawan772@gmail.com>
2025-06-11 07:16:50 +0700

    Restructure user feature

--- lib/features/authentication/presentation/riverpod/user_form_provider.dart
+++ lib/features/authentication/presentation/riverpod/user_form_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pockaw/features/authentication/data/local/user_repository.dart';
-import 'package:pockaw/features/authentication/domain/models/user_model.dart';
+import 'package:pockaw/features/authentication/data/repositories/user_repository.dart';
+import 'package:pockaw/features/authentication/data/models/user_model.dart';
 
 final userProvider = StateProvider<UserModel>((ref) {
   return UserRepository.dummy;
--- lib/features/authentication/presentation/screens/login_screen.dart
+++ lib/features/authentication/presentation/screens/login_screen.dart
@@ -16,7 +16,7 @@ import 'package:pockaw/core/router/routes.dart';
 import 'package:pockaw/core/services/image_service/domain/image_state.dart';
 import 'package:pockaw/core/services/image_service/image_service.dart';
 import 'package:pockaw/core/services/image_service/riverpod/image_notifier.dart';
-import 'package:pockaw/features/authentication/domain/models/user_model.dart';
+import 'package:pockaw/features/authentication/data/models/user_model.dart';
 import 'package:pockaw/features/authentication/presentation/riverpod/auth_provider.dart';
 import 'package:pockaw/features/currency_picker/presentation/components/currency_picker_field.dart';
 import 'package:pockaw/features/currency_picker/presentation/riverpod/currency_picker_provider.dart';
@@ -48,9 +48,7 @@ class LoginScreen extends HookConsumerWidget {
               children: [
                 const Logo(),
                 const Gap(AppSpacing.spacing48),
-                Form(
-                  nameField: nameField,
-                ),
+                Form(nameField: nameField),
               ],
             ),
           ),
@@ -80,7 +78,7 @@ class LoginScreen extends HookConsumerWidget {
                 },
               ),
             ),
-          )
+          ),
         ],
       ),
     );
