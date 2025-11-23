8d29c03597b2f0bca262035c4f611e96a362686f
Agil Setiawan <agilsetiawan772@gmail.com>
2025-11-23 18:28:38 +0700

    Fix desktop build

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pockaw/core/app.dart';
-// hooks_riverpod import removed; widgets should import flutter_riverpod or hooks_riverpod where needed
 import 'package:pockaw/firebase_options.dart';
 
 Future<void> main() async {
